6a316f8f02c042be4d2f4c8c912f30d820ded81e
Julien Giovaresco <dev@giovaresco.fr>
2025-10-20 13:48:05 +0200

    fix(controller): handle WebSocket authentication asynchronously
    Refactored WebSocketRequestHandler to use RxJava for asynchronous
    handling of authentication and WebSocket connection.
    
    Separated logic for handling valid connections into a dedicated method
    for better code readability and maintainability.
    
    https://gravitee.atlassian.net/browse/APIM-11656

--- gravitee-exchange-controller/gravitee-exchange-controller-websocket/src/main/java/io/gravitee/exchange/controller/websocket/WebSocketRequestHandler.java
+++ gravitee-exchange-controller/gravitee-exchange-controller-websocket/src/main/java/io/gravitee/exchange/controller/websocket/WebSocketRequestHandler.java
@@ -32,7 +32,9 @@ import io.gravitee.exchange.api.websocket.command.ExchangeSerDe;
 import io.gravitee.exchange.api.websocket.protocol.ProtocolVersion;
 import io.gravitee.exchange.controller.websocket.auth.WebSocketControllerAuthentication;
 import io.gravitee.exchange.controller.websocket.channel.WebSocketControllerChannel;
-import io.gravitee.node.api.Node;
+import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import io.vertx.rxjava3.core.RxHelper;
 import io.vertx.rxjava3.core.Vertx;
 import io.vertx.rxjava3.core.http.HttpServerRequest;
 import io.vertx.rxjava3.ext.web.RoutingContext;
@@ -63,49 +65,68 @@ public class WebSocketRequestHandler implements io.vertx.core.Handler<io.vertx.r
 
         log.debug("Incoming connection on Websocket Controller");
         HttpServerRequest request = routingContext.request();
-        ControllerCommandContext controllerContext = controllerAuthentication.authenticate(request);
-        if (controllerContext.isValid()) {
-            // Resolve protocol version from header
-            String headerValue = request.getHeader(EXCHANGE_PROTOCOL_HEADER);
-            ProtocolVersion protocolVersion = ProtocolVersion.parse(headerValue);
+        request.pause();
 
-            request
-                .toWebSocket()
-                .flatMapCompletable(webSocket -> {
-                    List<CommandHandler<? extends Command<?>, ? extends Reply<?>>> commandHandlers =
-                        controllerCommandHandlersFactory.buildCommandHandlers(controllerContext);
-                    List<CommandAdapter<? extends Command<?>, ? extends Command<?>, ? extends Reply<?>>> commandAdapters =
-                        controllerCommandHandlersFactory.buildCommandAdapters(controllerContext, protocolVersion);
-                    List<ReplyAdapter<? extends Reply<?>, ? extends Reply<?>>> replyAdapters =
-                        controllerCommandHandlersFactory.buildReplyAdapters(controllerContext, protocolVersion);
+        Single
+            .fromCallable(() -> controllerAuthentication.authenticate(request))
+            .subscribeOn(RxHelper.blockingScheduler(vertx))
+            .observeOn(RxHelper.scheduler(vertx))
+            .subscribe(
+                controllerContext -> {
+                    if (controllerContext.isValid()) {
+                        handleValidConnection(routingContext, request, controllerContext);
+                    } else {
+                        // Authentication failed so reject the request
+                        log.debug("Unauthorized request on Websocket Controller");
+                        routingContext.fail(HttpStatusCode.UNAUTHORIZED_401);
+                    }
+                },
+                throwable -> {
+                    log.error("Unexpected error when authenticating incoming connection", throwable);
+                    routingContext.fail(HttpStatusCode.INTERNAL_SERVER_ERROR_500);
+                }
+            );
+    }
 
-                    ControllerChannel websocketControllerChannel = new WebSocketControllerChannel(
-                        commandHandlers,
-                        commandAdapters,
-                        replyAdapters,
-                        vertx,
-                        webSocket,
-                        protocolVersion.adapterFactory().apply(commandSerDe)
-                    );
-                    return exchangeController
-                        .register(websocketControllerChannel)
-                        .doOnComplete(() ->
-                            webSocket.closeHandler(v ->
-                                exchangeController.unregister(websocketControllerChannel).onErrorComplete().subscribe()
-                            )
-                        )
-                        .doOnError(throwable -> {
-                            log.error("Unable to register websocket channel");
-                            webSocket.close((short) 1011, "Unexpected error while registering channel").subscribe();
-                        })
-                        .onErrorComplete();
-                })
-                .doOnError(throwable -> routingContext.fail(HttpStatusCode.INTERNAL_SERVER_ERROR_500))
-                .subscribe();
-        } else {
-            // Authentication failed so reject the request
-            log.debug("Unauthorized request on Websocket Controller");
-            routingContext.fail(HttpStatusCode.UNAUTHORIZED_401);
-        }
+    private void handleValidConnection(
+        RoutingContext routingContext,
+        HttpServerRequest request,
+        ControllerCommandContext controllerContext
+    ) {
+        // Resolve protocol version from header
+        String headerValue = request.getHeader(EXCHANGE_PROTOCOL_HEADER);
+        ProtocolVersion protocolVersion = ProtocolVersion.parse(headerValue);
+
+        request
+            .toWebSocket()
+            .flatMapCompletable(webSocket -> {
+                List<CommandHandler<? extends Command<?>, ? extends Reply<?>>> commandHandlers =
+                    controllerCommandHandlersFactory.buildCommandHandlers(controllerContext);
+                List<CommandAdapter<? extends Command<?>, ? extends Command<?>, ? extends Reply<?>>> commandAdapters =
+                    controllerCommandHandlersFactory.buildCommandAdapters(controllerContext, protocolVersion);
+                List<ReplyAdapter<? extends Reply<?>, ? extends Reply<?>>> replyAdapters =
+                    controllerCommandHandlersFactory.buildReplyAdapters(controllerContext, protocolVersion);
+
+                ControllerChannel websocketControllerChannel = new WebSocketControllerChannel(
+                    commandHandlers,
+                    commandAdapters,
+                    replyAdapters,
+                    vertx,
+                    webSocket,
+                    protocolVersion.adapterFactory().apply(commandSerDe)
+                );
+                return exchangeController
+                    .register(websocketControllerChannel)
+                    .doOnComplete(() ->
+                        webSocket.closeHandler(v -> exchangeController.unregister(websocketControllerChannel).onErrorComplete().subscribe())
+                    )
+                    .doOnError(throwable -> {
+                        log.error("Unable to register websocket channel");
+                        webSocket.close((short) 1011, "Unexpected error while registering channel").subscribe();
+                    })
+                    .onErrorComplete();
+            })
+            .doOnError(throwable -> routingContext.fail(HttpStatusCode.INTERNAL_SERVER_ERROR_500))
+            .subscribe();
     }
 }
